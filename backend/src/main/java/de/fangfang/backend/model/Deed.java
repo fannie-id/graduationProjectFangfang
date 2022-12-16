425b1c7f7c9800dc01f58947aa9dd8ca7982dc13
fangfangwang <fannie-id@hotmail.com>
2022-12-16 15:27:20 +0100

    dix local-datenbank verbindung

--- backend/src/main/java/de/fangfang/backend/model/Deed.java
+++ backend/src/main/java/de/fangfang/backend/model/Deed.java
@@ -2,9 +2,9 @@ package de.fangfang.backend.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("deeds")
+@Document("Deeds")
 public record Deed(
-        //String id,
+        String id,
         String description,
         Address address,
         int karmaPoint
