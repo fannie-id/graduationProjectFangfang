4b65cb9c0fe4de66090376a9eb95b18e41f1c48a
fangfangwang <fannie-id@hotmail.com>
2022-12-22 21:36:56 +0100

    Add Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import DeedApp from "./components/DeedApp";
+import DeedDetail from "./components/DeedDetail";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-        <DeedApp/>
-    </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/allDeeds" element={<DeedApp/>}></Route>
+                    <Route path="/allDeeds/:id" element={<DeedDetail/>}></Route>
+                </Routes>
+            </BrowserRouter>
+            <DeedApp/>
+        </div>
   );
 }
 
