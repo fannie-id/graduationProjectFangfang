68fbbfe03520a6fbd3f9896282126aed7299e0b9
fangfangwang <fannie-id@hotmail.com>
2023-01-03 13:35:22 +0100

    Add DeedDetail, add useDeed

---
+++ frontend/src/hooks/useDeed.ts
@@ -0,0 +1,37 @@
+import {Deed} from "../model/Deed";
+import {useEffect, useState} from "react";
+import {getDeedById} from "../api/api-calls";
+
+export default function useDeed(id: string | undefined) {
+
+    const emptyDeed: Deed = {
+        id: "",
+        description: "",
+        address: {
+            street: "",
+            houseNumber: "",
+            zip: "",
+            city: "",
+            name: ""
+        },
+        karmaPoints: 0
+    }
+
+    const [getDeed, setGetDeed] = useState<Deed>(emptyDeed)
+    useEffect(() => {
+        if (id) {
+            getDeedViaId(id)
+        }
+    }, [])
+
+    function getDeedViaId(id: string) {
+        getDeedById(id)
+            .then(data => {
+                setGetDeed(data)
+            })
+            .catch(console.error)
+    }
+
+    return {getDeed}
+
+}
