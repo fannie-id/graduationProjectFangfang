5f42600fb16b36d22e69fd0863e38e8e18c7d5d6
fangfangwang <fannie-id@hotmail.com>
2023-01-03 13:35:22 +0100

    Add DeedDetail, add useDeed

--- frontend/src/components/DeedDetail.tsx
+++ frontend/src/components/DeedDetail.tsx
@@ -1,7 +1,17 @@
+import {useParams} from "react-router-dom";
+import useDeed from "../hooks/useDeed";
+
 export default function DeedDetail() {
+    const {id} = useParams()
+    const {getDeed} = useDeed(id)
+    if (!getDeed) {
+        return <p>loading</p>
+    }
+
+
     return (
         <div>
-
+            <p>{getDeed.description}</p>
         </div>
     )
 
