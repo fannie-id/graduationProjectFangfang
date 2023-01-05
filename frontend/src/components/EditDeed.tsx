b7eaa168b35fb322582b9ec4284a87374f2903a0
fangfangwang <fannie-id@hotmail.com>
2023-01-05 13:37:39 +0100

    add navigate to deeds after edit Deed

--- frontend/src/components/EditDeed.tsx
+++ frontend/src/components/EditDeed.tsx
@@ -1,28 +1,26 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import useDeed from "../hooks/useDeed";
 import DeedForm from "./DeedForm";
 import {Deed, NewDeed} from "../model/Deed";
 
 export default function EditDeed() {
-
+    const navigate = useNavigate()
     const {id} = useParams()
     if (!id) {
         return <p>id not found</p>
     }
-    const {getDeed, editDeed} = useDeed(id)
 
+    const {getDeed, editDeed} = useDeed(id)
 
     if (!getDeed) {
         return <p>loading</p>
     }
 
-
     function submitDeed(deed: Deed | NewDeed) {
-
         if ("id" in deed) {
             editDeed(deed)
+            navigate("/deeds")
         }
-
     }
 
     return (
