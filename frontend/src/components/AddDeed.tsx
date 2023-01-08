731ec1d7eaee9a952e80ce724df941267da41592
fangfangwang <fannie-id@hotmail.com>
2023-01-08 15:28:16 +0100

    Add navigate to deeds after add a deed

--- frontend/src/components/AddDeed.tsx
+++ frontend/src/components/AddDeed.tsx
@@ -3,10 +3,11 @@ import {NewDeed} from "../model/Deed";
 import {Address} from "../model/Address";
 import DeedForm from "./DeedForm";
 import useDeeds from "../hooks/useDeeds";
+import {useNavigate} from "react-router-dom";
 
 
 export default function AddDeed() {
-
+    const navigate = useNavigate()
     const {addNewDeed} = useDeeds()
     const address: Address = {
         "street": "",
@@ -24,6 +25,7 @@ export default function AddDeed() {
 
     function submitDeed(deed: NewDeed) {
         addNewDeed(deed)
+        navigate("/deeds")
     }
 
     return (
