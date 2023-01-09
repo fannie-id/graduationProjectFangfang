b63765967ca706029f63d2955bc53b5ac9f51913
fangfangwang <fannie-id@hotmail.com>
2023-01-09 09:25:40 +0100

    Add User-record

---
+++ backend/src/main/java/de/fangfang/backend/model/User.java
@@ -0,0 +1,18 @@
+package de.fangfang.backend.model;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+public record User(
+        @Id
+        String id,
+        String name,
+        String email,
+        String password,
+        List<String> givenDeeds,
+        List<String> takenDeeds,
+        Address address,
+        int karmaPoints
+) {
+}
