11631a5226c3be426829cc27e8b8679e1d494522
fangfangwang <fannie-id@hotmail.com>
2023-01-12 09:06:47 +0100

    Add GlobalExceptionHandler

---
+++ backend/src/main/java/de/fangfang/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package de.fangfang.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IdNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleIdNotFoundException(IdNotFoundException exception) {
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("message", exception.getMessage());
+        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneralException() {
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("message", "bad request");
+        return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
+    }
+}
