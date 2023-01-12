e551a1791db9f7fd0c6196d78e38b0a1d516328d
fangfangwang <fannie-id@hotmail.com>
2023-01-12 20:21:34 +0100

    add userController

--- backend/src/main/java/de/fangfang/backend/security/UserController.java
+++ backend/src/main/java/de/fangfang/backend/security/UserController.java
@@ -1,16 +1,16 @@
 package de.fangfang.backend.security;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 public class UserController {
     @GetMapping("me")
     public String helloMe(Principal principal) {
-        return "Hello" + principal.getName();
+        return "Hello " + principal.getName();
     }
 }
