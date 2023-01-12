3b2e738a7a8d8f9bee1283f27ffe73eab9768d23
fangfangwang <fannie-id@hotmail.com>
2023-01-12 18:27:22 +0100

    add Argon2PasswordEncoder

--- backend/src/main/java/de/fangfang/backend/security/MongoUser.java
+++ backend/src/main/java/de/fangfang/backend/security/MongoUser.java
@@ -3,7 +3,7 @@ package de.fangfang.backend.security;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("mongoUser")
+@Document("mongoUsers")
 public record MongoUser(
         @Id
         String id,
--- backend/src/main/java/de/fangfang/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/fangfang/backend/security/SecurityConfig.java
@@ -6,27 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    /*    @Bean
-        public InMemoryUserDetailsManager userDetailsManager() {
-            return new InMemoryUserDetailsManager(
-                    User.builder()
-                            .username("max")
-                            .password("max")
-                            .roles("ADMIN")
-                            .build()
-            );
-        }
-    */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
 
     @Bean
