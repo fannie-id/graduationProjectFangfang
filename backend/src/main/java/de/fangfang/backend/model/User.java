bfdc8df3639a2fc9a4f52de90f5b8d62fe28330e
fangfangwang <fannie-id@hotmail.com>
2023-01-13 17:08:05 +0100

    add User, UserDTO

---
+++ backend/src/main/java/de/fangfang/backend/model/User.java
@@ -0,0 +1,22 @@
+package de.fangfang.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("mongoUsers")
+public record User(
+        @Id
+        String id,
+        @Indexed(unique = true)
+        String username,
+        String password,
+        String email,
+        List<String> givenDeeds,
+        List<String> takenDeeds,
+        Address address,
+        int karmaPoints
+) {
+}
