7607a001122dd2f6a1843e416ebc25e4e9283868
fangfangwang <fannie-id@hotmail.com>
2023-01-14 21:57:21 +0100

    fix encode Bug in servic & test

--- backend/src/main/java/de/fangfang/backend/controller/UserController.java
+++ backend/src/main/java/de/fangfang/backend/controller/UserController.java
@@ -1,16 +1,29 @@
 package de.fangfang.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.fangfang.backend.model.UserRegistration;
+import de.fangfang.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @GetMapping("me")
     public String helloMe(Principal principal) {
         return "Hello " + principal.getName();
+    }
+
+    @PostMapping
+    public void registerUser(@RequestBody UserRegistration newUser) {
+        userService.registerNewUser(newUser);
     }
 }
