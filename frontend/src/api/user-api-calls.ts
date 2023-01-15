75b25abd0c9a36def5cd5647ec91d7600b864dad
fangfangwang <fannie-id@hotmail.com>
2023-01-15 15:49:51 +0100

    clean code

--- frontend/src/api/user-api-calls.ts
+++ frontend/src/api/user-api-calls.ts
@@ -4,12 +4,6 @@ import {RegisterUser} from "../model/User";
 
 const userEndPoint: string = "/api/users";
 
-// export function getUsers(): Promise<User[]> {
-export function getUsers() {
-    return axios.get(userEndPoint)
-        .then(response => response.data)
-}
-
 export function createUser(newUser: RegisterUser): Promise<Deed> {
     return axios.post(userEndPoint, newUser)
 }
--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -2,12 +2,6 @@ import {createUser} from "../api/user-api-calls";
 import {RegisterUser} from "../model/User";
 
 export default function useUsers() {
-    /*const[users,setUsers]=useState([])
-    useEffect(() => {
-        getUsers()
-            .then(data => setUsers(data))
-            .catch(console.error)
-    }, [])*/
 
     function addUser(newUser: RegisterUser) {
         createUser(newUser)
