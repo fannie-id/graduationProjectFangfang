b3911cee1059414c44a418d3d581e726d634d31e
Fang² <fannie-id@hotmail.com>
2023-01-15 16:57:07 +0100

    Update user-api-calls.ts

--- frontend/src/api/user-api-calls.ts
+++ frontend/src/api/user-api-calls.ts
@@ -4,6 +4,6 @@ import {RegisterUser} from "../model/User";
 
 const userEndPoint: string = "/api/users";
 
-export function createUser(newUser: RegisterUser): Promise<Deed> {
+export function createUser(newUser: RegisterUser){
     return axios.post(userEndPoint, newUser)
 }
