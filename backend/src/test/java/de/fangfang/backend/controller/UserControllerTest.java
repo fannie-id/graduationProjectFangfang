7e3a51ee7a0fcf9d2abdfe0a9bab3a24210cdf26
fangfangwang <fannie-id@hotmail.com>
2023-01-17 15:33:58 +0100

    fix merging bug

--- backend/src/test/java/de/fangfang/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/fangfang/backend/controller/UserControllerTest.java
@@ -59,7 +59,7 @@ class UserControllerTest {
                                 },
                                 "karmaPoints":0
                                 }
-                                """)
+                                """).with(csrf())
                 )
                 .andExpect(status().isOk());
     }
