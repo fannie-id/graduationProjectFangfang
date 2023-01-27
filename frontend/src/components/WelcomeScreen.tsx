517e3fdcd249fd9d2546638fd2ef8a1622e3a164
fangfangwang <fannie-id@hotmail.com>
2023-01-27 13:21:13 +0100

    float btn

--- frontend/src/components/WelcomeScreen.tsx
+++ frontend/src/components/WelcomeScreen.tsx
@@ -1,48 +1,60 @@
 import './WelcomeScreen.css';
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
-import {Box, Button} from "@mui/material";
+import {Box, Fab} from "@mui/material";
 import FaceIcon from "@mui/icons-material/Face";
+import {useNavigate} from "react-router-dom";
+import React from "react";
 
 export default function WelcomeScreen() {
+    const navigate = useNavigate()
+
+    function login() {
+        navigate("/login")
+        console.log("login")
+    }
+
+    function register() {
+        navigate("/register")
+    }
 
 
     return (<div>
             <div className="context">
-                <Box sx={{pb: 7}}
-                     mt={"6"}
-                     margin={"35px"}
-                     flexDirection={"column"}
-                     display={"flex"}
-                     flexWrap={"wrap"}
-                     alignItems="center"
-                     justifyContent={"center"}>
+                <Box
+                    flexDirection={"column"}
+                    display={"flex"}
+                    flexWrap={"wrap"}
+                    alignItems="center"
+                    justifyContent={"center"}>
 
 
                     <h2>welcome!</h2>
-                    <Button sx={{mt: 2}}
-                            variant="contained"
-                            type="submit"
-                            color="success"
-                            size="large"
-                            href="/login"
-                            startIcon={<FaceIcon sx={{width: 30, height: 30}}/>}>
-                        login
-                    </Button>
 
-                    <Button sx={{mt: 4, frontSize: "20px"}}
-                            variant="contained"
-                            type="submit"
-                            color="success"
-                            size="large"
-                            href="/register"
-                            startIcon={<PersonAddAlt1Icon sx={{width: 30, height: 30}}/>}>
+
+                    <Fab color="success"
+                         style={{position: "absolute", top: 80}}
+                         aria-label="add"
+                         onClick={login}
+                         variant="extended">
+                        <FaceIcon/>
+                        login
+                    </Fab>
+
+                    <Fab color="success"
+                         style={{position: "absolute", top: 160}}
+                         aria-label="add"
+                         onClick={register}
+                         variant="extended">
+                        <PersonAddAlt1Icon/>
                         register
-                    </Button>
+                    </Fab>
+
+
                 </Box>
             </div>
 
             <div className="area">
-                <ul className="circles">
+                <ul className="bubble">
                     <li></li>
                     <li></li>
                     <li></li>
