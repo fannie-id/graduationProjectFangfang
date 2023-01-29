49defb440dbbabf4c482781a4f4d6372c83bffe6
fangfangwang <fannie-id@hotmail.com>
2023-01-29 20:05:05 +0100

    test fix endless loop

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -29,7 +29,7 @@ export default function useUser() {
                 .then(setLoggedInUser)
         }
 
-    }, [loggedInUser])
+    }, [])
 
     function getLoginUser(user: UserLogin): Promise<UserInfo> {
         return loginUser(user)
